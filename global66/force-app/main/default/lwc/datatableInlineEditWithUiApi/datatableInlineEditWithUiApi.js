6fa8da1fcde1033b47d839c4d4f2aa9815b594f2
Ignacio Navas <dysrupterwow@gmail.com>
2024-12-30 13:52:45 +0000

    Casi andando

--- global66/force-app/main/default/lwc/datatableInlineEditWithUiApi/datatableInlineEditWithUiApi.js
+++ global66/force-app/main/default/lwc/datatableInlineEditWithUiApi/datatableInlineEditWithUiApi.js
@@ -1,64 +1,46 @@
 import { LightningElement, wire } from 'lwc';
 import rContacts from '@salesforce/apex/ContactManagementSystemController.rContacts';
 import { refreshApex } from '@salesforce/apex';
-import { updateRecord } from 'lightning/uiRecordApi';
-
+import { deleteRecord } from 'lightning/uiRecordApi';  // Importar deleteRecord
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
-import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
-import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
-import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 
-const COLS = [
-    {
-        label: 'First Name',
-        fieldName: FIRSTNAME_FIELD.fieldApiName,
-        editable: true
-    },
-    {
-        label: 'Last Name',
-        fieldName: LASTNAME_FIELD.fieldApiName,
-        editable: true
-    },
-    {
-        label: 'Phone',
-        fieldName: PHONE_FIELD.fieldApiName,
-        type: 'phone',
-        editable: true
-    },
-    {
-        label: 'Email',
-        fieldName: EMAIL_FIELD.fieldApiName,
-        type: 'email',
-        editable: true
-    }
+
+const actions = [
+    { label: 'Show details', name: 'show_details' },
+    { label: 'Delete', name: 'delete' }
 ];
+const COLS = [
+    { label: 'First Name', fieldName: 'FirstName' ,editable : 'true',sortable: true },
+    { label: 'Last Name', fieldName: 'LastName',editable : 'true' },
+    { label: 'Phone', fieldName: 'Phone', type: 'phone' ,editable : 'true'},
+    { label: 'Email', fieldName: 'Email', type: 'email' ,editable : 'true'},
+    { type: 'action', typeAttributes: { rowActions: actions, menuAlignment: 'left' } }
+];
+
+
 export default class DatatableInlineEditWithUiApi extends LightningElement {
     columns = COLS;
     draftValues = [];
-
-    // Using Apex to fetch records while waiting for a replacement to getListUi() which is deprecated
+    selectedRecords = []; 
+    loadMoreStatus;
     @wire(rContacts)
     contacts;
+    btnDelete = false;
 
     async handleSave(event) {
-        // Convert datatable draft values into record objects
         const records = event.detail.draftValues.slice().map((draftValue) => {
             const fields = Object.assign({}, draftValue);
             return { fields };
         });
 
-        // Clear all datatable draft values
         this.draftValues = [];
 
         try {
-            // Update all records in parallel thanks to the UI API
             const recordUpdatePromises = records.map((record) =>
                 updateRecord(record)
             );
             await Promise.all(recordUpdatePromises);
 
-            // Report success with a toast
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Success',
@@ -67,7 +49,6 @@ export default class DatatableInlineEditWithUiApi extends LightningElement {
                 })
             );
 
-            // Display fresh data in the datatable
             await refreshApex(this.contacts);
         } catch (error) {
             this.dispatchEvent(
@@ -79,4 +60,69 @@ export default class DatatableInlineEditWithUiApi extends LightningElement {
             );
         }
     }
+
+    getSelectedName(event) {
+        const selectedRows = event.detail.selectedRows;
+        // Display that fieldName of the selected rows
+        for (let i = 0; i < selectedRows.length; i++) {
+            alert('You selected: ' + selectedRows[i].Id);
+        }
+    }
+
+    loadMoreData(event) {
+        event.target.isLoading = true;
+        this.loadMoreStatus = ' ';
+        rContacts(10).then((data) => {
+            if (data.length >= this.totalNumberOfRows) {
+                event.target.enableInfiniteLoading = false;
+                this.loadMoreStatus = 'No more data to load';
+            } else {
+                const currentData = this.data;
+                //Appends new data to the end of the table
+                const newData = currentData.concat(data);
+                this.data = newData;
+                this.loadMoreStatus = '';
+            }
+            event.target.isLoading = false;
+        });
+    }
+
+    handleSelect(event) {
+        const selectedRows = event.detail.selectedRows;
+        for (let i = 0; i < selectedRows.length; i++) {
+            alert('You selected: ' + selectedRows[i].Id);
+        }
+
+    }
+    handleRowAction(event) {
+        const action = event.detail.action;
+        const row = event.detail.row;
+        switch (action.name) {
+            case 'show_details':
+                alert('Showing Details: ' + JSON.stringify(row));
+                break;
+            case 'delete':
+                const rows = this.data;
+                const rowIndex = rows.indexOf(row);
+                rows.splice(rowIndex, 1);
+                this.data = rows;
+                break;
+        }
+    }
+
+
+   updateColumnSorting(event) {
+        try{
+            var fieldName = event.detail.fieldName;
+            var sortDirection = event.detail.sortDirection;
+            this.sortedBy = fieldName;
+            this.sortedDirection = sortDirection;
+            this.data = this.sortData(fieldName, sortDirection);
+        }
+        catch (error) {
+            console.error('Error en la ordenación:', error);
+        }
+    } 
+    
+
 }
